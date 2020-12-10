1fdbea11b2205ee6b9f21831b4249a388d9c5295
Raihanraaz001 <59445100+Raihanraaz001@users.noreply.github.com>
2020-12-10 11:33:24 +0600

    Update index.js

--- tarfi-birthday/scripts/index.js
+++ tarfi-birthday/scripts/index.js
@@ -48,8 +48,8 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY BIRTHDAY!', 'TO MY DEAR FRIEND', config.name],
-      charSize: 30,
+      strings: [  'HAPPY BIRTHDAY!',  'TO MY DEAR FRIEND', config.name],
+      charSize: 25,
       charSpacing: 25,
       lineHeight: 30,
 
